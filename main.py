e787eab90a1f91c17c4b3f24f775372ad618006a
Gustavo Corrêa Mendes <67390202+gustavomendesof@users.noreply.github.com>
2023-02-06 22:03:45 -0300

    uptoDate

---
+++ main.py
@@ -0,0 +1,8 @@
+# Primeiro exemplo de manipulação de dados usando python pandas
+#python -m pip install pandas - (comando para o console)
+
+#Importanto biblioteca pandas no python
+import pandas as pd
+
+combustiveis_df = pd.read_csv("precos-semestrais-ca-2022-01")
+
