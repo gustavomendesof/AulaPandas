7b7468d5020df891bc86d5cf42a76b794e61595d
Gustavo Corrêa Mendes <67390202+gustavomendesof@users.noreply.github.com>
2023-02-07 21:58:05 -0300

    UpToDate

--- Aula-02/aula02.py
+++ Aula-02/aula02.py
@@ -4,10 +4,7 @@
 #Importanto biblioteca pandas no python
 import pandas as pd
 
-
 combustiveis_df = pd.read_excel("ca-2021-02.xlsx")
-
-
 
 #Usa o display  para ver o dataframe
 display(combustiveis_df)
@@ -31,3 +28,29 @@ print(combustiveis_df.info())
 
 #Describe() mostra as estatisticas mais basicas que a gente precisa
 print(combustiveis_df.describe())
+
+#Filtrar apenas por uma coluna
+display(combustiveis_df['Revenda'])
+
+#Aqui criamos um novo dataframe apenas com as colunas que eu quero
+ca_df = combustiveis_df[['Revenda','Municipio', 'Produto','Valor de Venda']]
+
+display(ca_df)
+
+#Exibe a 4a. linha
+display(ca_df.loc[3])
+
+#Exibir da 10a. linha até a 20a.linha
+display(ca_df.loc[9:19])
+
+#Criar uma data frama gas_df contendo apenas 4 colunas (Revenda, Municipio, Produdo, valor de venda)
+#somente com combustivel sendo gasolina e exibir na tela
+gas_df = ca_df.loc[ca_df['Produto'] == 'GASOLINA']
+display(gas_df)
+display(gas_df['Valor de Venda'].max())
+
+
+#Se eu quisar saber qual é o posto e municipio da gasolina mais cara do pais?
+display(gas_df[['Revenda', 'Municipio', 'Valor de Venda']].max())
+
+
